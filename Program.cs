c22a53caed603e30d2249430dccc2377a83426b3
dinooo <145918377+dindooo@users.noreply.github.com>
2023-11-15 09:28:47 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ispitCetvrti1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Upisite a stranicu");
+            a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Upisite b stranicu");
+            b=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Upisite c stranicu");
+            c=Convert.ToInt32(Console.ReadLine());
+            if (a*a+b*b==c*c)
+            {
+                Console.WriteLine("Trokut je pravokutan");
+            }
+            else
+            {
+                Console.WriteLine("Nije pravokutan");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
